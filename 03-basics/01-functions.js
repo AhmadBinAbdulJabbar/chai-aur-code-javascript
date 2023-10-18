1be76aacbc9b099cc76674a046e729469c74b7bb
ahmadbinabduljabbar <ahmadbinabduljabbar@gmail.com>
2023-10-18 18:16:01 +0000

    more on functions (arrays and objects passing)

--- 03-basics/01-functions.js
+++ 03-basics/01-functions.js
@@ -42,3 +42,33 @@ function loginUserMessage(username = "sam"){
 // console.log(loginUserMessage("ahmad"));
 // console.log(loginUserMessage()); // undefined is returned not null
 
+function calculateCartPrice(val1, val2, ...num1){
+    return num1;
+}
+
+console.log(calculateCartPrice(200, 400, 500, 2000));
+
+const user = {
+    username: "ahmad",
+    price: 199,
+}
+
+function handleObject(anyObject){
+    console.log(`username is ${anyObject.username} and price is ${anyObject.price}`);
+};
+
+// handleObject(user);
+handleObject({
+    username: "ali",
+    price: 399,
+})
+
+const myNewArray = [200, 400, 100, 600];
+
+function returnSecondValue(getArray){
+    return getArray[1];
+}
+
+console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([300,500,1000]));
+
