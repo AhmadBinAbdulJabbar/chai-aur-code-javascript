0dc5699fa170114e912dce53b1736f973dd3931a
ahmadbinabduljabbar <ahmadbinabduljabbar@gmail.com>
2023-10-22 08:04:46 +0000

    while do while loop

---
+++ 05-iterations/two.js
@@ -0,0 +1,23 @@
+
+let index = 0;
+while (index <= 10) {
+    console.log(`value of index is ${index}`);
+    index += 2;
+}
+
+
+let myArray = ["flash","batman", "superman"];
+
+let arrIndex = 0;
+while(arrIndex < myArray.length){
+    console.log(`value is ${myArray[arrIndex]}`);
+    arrIndex = arrIndex + 1;
+}
+
+// let score = 1;
+let score = 11;
+
+do {
+    console.log(`Score is ${score}`);
+    score++;
+} while (score <= 10);
