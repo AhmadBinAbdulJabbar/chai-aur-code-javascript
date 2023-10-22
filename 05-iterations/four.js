2daea100293bd7ef03831a4534136d8ea815cc19
ahmadbinabduljabbar <ahmadbinabduljabbar@gmail.com>
2023-10-22 09:44:58 +0000

    forin loop

---
+++ 05-iterations/four.js
@@ -0,0 +1,28 @@
+const myObject = {
+    js: 'javascirpt',
+    cpp: 'C++',
+    rb: "ruby",
+    swift: 'swift by apple'
+}
+
+// for (const key in myObject) {
+//     // console.log(myObject[key]);
+//     console.log(`${key} is short for ${myObject[key]}`);
+// }
+
+// const programming = ["js", "cpp", "py", "java", "rb"];
+// for (const key in programming) {
+//     console.log(programming[key]);
+// }
+
+
+const map = new Map();
+map.set('IN', "India");
+map.set('USA', "United States of America");
+map.set('FR', "France");
+
+// not iterable like this
+// for (const key in map) {
+//    console.log(key);
+// }
+
